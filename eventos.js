2a374f9bfadfb84f34f11064764b11f2e0d3ac39
¨Braian¨ <braiansilva4@gmail.com>
2025-08-11 19:27:10 -0300

    Correccion de bubbling

--- eventos.js
+++ eventos.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
     
     const miDiv = document.getElementById('miDiv');
+    const boton = document.getElementById('saludar');
 
     miDiv.addEventListener('click', function() {
         alert("Hola! Soy el div");
-    })
-})
+    });
+
+    boton.addEventListener('click', function(event){
+        event.stopPropagation();
+    });
+});
